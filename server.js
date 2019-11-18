231d94fa6d969bbb48ad4644a59c7ea09f7035f8
Adarsh Verma <adarshverma11236@gmail.com>
2019-11-18 14:50:33 +0200

    change port

--- server.js
+++ server.js
@@ -107,6 +107,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, postgres) })
 })*/
 //////////////////////////////////////////////////////////////////1
 
-app.listen(5505, () => {
-  console.log('apppppppppppppppp  listeninh8')
+app.listen(process.env.PORT || 5505, () => {
+  console.log('apppppppppppppppp  listeninh9 ${process.env.PORT}')
 })
