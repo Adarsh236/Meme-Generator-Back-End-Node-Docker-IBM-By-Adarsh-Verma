f14d5855c2d37e99908259f278dacede57d8dfce
adarshverma236 <adarshverma11236@gmail.com>
2020-02-17 23:02:17 +0200

    formatting code

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ const postgres = knex({
 
 
 const app = express();
-//console.log('I love yooooooooooou?')
 app.use(morgan('combined'))
 app.use(cors())
 app.use(bodyParser.json());
@@ -34,77 +33,6 @@ app.post('/signin', (req, res) => { signin.handleSignin(req, res, postgres, bcry
 app.post('/register', (req, res) => { register.handleRegister(req, res, postgres, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, postgres) })
 app.put('/image', (req, res) => { image.handleImage(req, res, postgres) })
-app.listen(process.env.PORT, () => {
-  console.log('apppppppppppppppp  listeninh9 ${process.env.PORT}')
+app.listen(process.env.PORT || 5505, () => {
+  console.log('server is running ${process.env.PORT}')
 })
-
-
-//console.log(process.env.POSTGRES_URI)
-/*console.log(process.env.POSTGRES_USER)
-console.log(process.env.POSTGRES_PASSWORD)
-console.log(process.env.POSTGRES_DB)
-console.log(process.env.POSTGRES_HOST)*/
-
-/*console.log(postgres.select('*').from('users').then(data => {
-  console.log(data);
-}));*/
-
-/*db.select('*').from('users').then(data =>{
-  console.log(data);
-});*/
-
-/*app.post('/signin', (req, res) => {
-  const {email, password} = req.body;
-  postgres.select('email', 'hash').from('login')
-      .where('email', '=', email)
-      .then(data => {
-        const isValid = bcrypt.compareSync(password, data[0].hash);
-        //console.log(isValid);
-        if (isValid) {
-          return postgres.select('*').from('users')
-            .where('email', '=', email)
-            .then(user => {
-           // console.log(user);
-              res.json(user[0])
-            })
-            .catch(err => res.status(400).json('unable to get user'))
-        }else{res.status(400).json('wrong credentials')}
-      }) 
-          .catch(err=>res.status(400).json('wrong credentials'))
-        
-      })*/
-
-/*app.post('/register', (req, res)=>{
-  const { email, name, password} = req.body;
-  postgres('users').returning('*').insert({
-email: email,
-name: name,
-joined: new Date()
-  
-} ).then(user=>{
-res.json(user[0]);}).catch(err=> res.status(400).json('unable to join'))  })
-*/
-
-/*app.get('/profile/:id', (req, res) =>{
-  const {id}= req.params;
-  postgres.select('*').from('users').where({
-      id
-    }).then(user =>{
-      if(user.length){
-      res.json(user[0])
-  }else{
-    res.status(400).json('not found')}
-  })
-.catch(err => res.status(400).json('error getting user'))
-  })*/
-
-/*app.put('/image', (req, res) => {
-  const{id}= req.body;
-  postgres('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  }).catch(err => res.status(400).json('unable to get entiries'))
-})*/
-//////////////////////////////////////////////////////////////////1
